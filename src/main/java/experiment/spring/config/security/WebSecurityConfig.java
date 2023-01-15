03c7923908e3181d55976fe2f5988b3edbdb99cb
kimminsu <xjfcnfw3@naver.com>
2023-01-15 14:01:06 +0900

    feat: 자동 세션 기능 해제

--- src/main/java/experiment/spring/config/security/WebSecurityConfig.java
+++ src/main/java/experiment/spring/config/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +13,12 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .csrf()
+            .disable();
         return http.build();
     }
 }
