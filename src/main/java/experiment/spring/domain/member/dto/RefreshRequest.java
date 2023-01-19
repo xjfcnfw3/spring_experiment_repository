68d6edda006513ae58f9920705718dd8a6510a79
kimminsu <xjfcnfw3@naver.com>
2023-01-19 16:15:46 +0900

    feat: refresh token api 기능 구현

---
+++ src/main/java/experiment/spring/domain/member/dto/RefreshRequest.java
@@ -0,0 +1,15 @@
+package experiment.spring.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshRequest {
+    private String refreshToken;
+    private String loginId;
+}
