2ca1d9df9f350b1f18136450579f0692271159b8
kimminsu <xjfcnfw3@naver.com>
2023-02-16 18:29:17 +0900

    refactor: 컨트롤러 반환 형식 변경

--- src/main/java/experiment/spring/controller/AuthController.java
+++ src/main/java/experiment/spring/controller/AuthController.java
@@ -1,17 +1,11 @@
 package experiment.spring.controller;
 
-
-import experiment.spring.domain.member.Member;
 import experiment.spring.domain.member.dto.LoginResponse;
-import experiment.spring.security.LoginMember;
-import experiment.spring.security.MemberDetails;
 import experiment.spring.security.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,9 +18,9 @@ public class AuthController {
     private final TokenProvider tokenProvider;
 
     @PostMapping("/refresh")
-    public ResponseEntity<LoginResponse> refresh(Authentication authentication) {
+    public LoginResponse refresh(Authentication authentication) {
         String refreshToken = tokenProvider.generateRefreshToken(authentication);
         String accessToken = tokenProvider.generateAccessToken(refreshToken);
-        return ResponseEntity.ok(new LoginResponse(accessToken, refreshToken));
+        return new LoginResponse(accessToken, refreshToken);
     }
 }
