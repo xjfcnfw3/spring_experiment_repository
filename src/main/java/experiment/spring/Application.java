d63f1f97852b14c601e4c6ef23b23f16a0df1fae
kimminsu <xjfcnfw3@naver.com>
2023-02-19 14:40:20 +0900

    feat: 스프링 배치 적용

--- src/main/java/experiment/spring/Application.java
+++ src/main/java/experiment/spring/Application.java
@@ -1,11 +1,13 @@
 package experiment.spring;
 
 import experiment.spring.config.security.AuthProperties;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableBatchProcessing
 @EnableConfigurationProperties(AuthProperties.class)
 public class Application {
 
