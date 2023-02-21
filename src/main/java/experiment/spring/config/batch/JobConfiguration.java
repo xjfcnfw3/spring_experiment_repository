b8cded48469b46233608ce42f6cce485a1953dd6
kimminsu <xjfcnfw3@naver.com>
2023-02-21 21:27:24 +0900

    feat: Parameter를 사용하는 Job 구현

--- src/main/java/experiment/spring/config/batch/JobConfiguration.java
+++ src/main/java/experiment/spring/config/batch/JobConfiguration.java
@@ -1,14 +1,13 @@
-package experiment.spring.config;
+package experiment.spring.config.batch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,7 +22,7 @@ public class JobConfiguration {
     public Job batchJob() {
         return jobBuilderFactory.get("job")
             .start(step1())
-            .next(step2())
+            .next(step2(null))
             .build();
     }
 
@@ -39,11 +38,12 @@ public class JobConfiguration {
     }
 
     @Bean
-    public Step step2() {
+    @JobScope
+    public Step step2(@Value("#{jobParameters['name']}") String name) {
         return stepBuilderFactory.get("step2")
             .tasklet(((contribution, chunkContext) -> {
                 System.out.println(" ============================");
-                System.out.println(" >> Step2 has executed");
+                System.out.println(" >> hello " + name);
                 System.out.println(" ============================");
                 return RepeatStatus.FINISHED;
             })).build();
---
+++ src/test/java/experiment/spring/batch/SpringBachTest.java
@@ -0,0 +1,31 @@
+package experiment.spring.batch;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBachTest {
+
+    @Autowired
+    Job job;
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Test
+    void runJob()
+        throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        Map<String, JobParameter> param = Map.of("name", new JobParameter("batch"));
+        jobLauncher.run(job, new JobParameters(param));
+    }
+}
