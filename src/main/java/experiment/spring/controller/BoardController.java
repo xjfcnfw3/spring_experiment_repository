d36d30a2a092d1e5f954edc6537b2341fa871686
kimminsu <xjfcnfw3@naver.com>
2023-03-05 16:08:45 +0900

    feat: 게시판 컨트롤러 구현

--- src/main/java/experiment/spring/controller/BoardController.java
+++ src/main/java/experiment/spring/controller/BoardController.java
@@ -2,13 +2,17 @@ package experiment.spring.controller;
 
 import experiment.spring.domain.Boaed.Board;
 import experiment.spring.domain.Boaed.BoardDto;
+import experiment.spring.domain.member.Member;
+import experiment.spring.security.LoginMember;
 import experiment.spring.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +25,10 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping
-    public Board save(@RequestBody BoardDto boardDto) {
-        return boardService.save(boardDto);
+    public Board save(@RequestBody BoardDto boardDto, @LoginMember Member member) {
+        return boardService.save(member, boardDto);
     }
 
     @GetMapping("/{id}")
@@ -31,8 +36,15 @@ public class BoardController {
         return boardService.getBoard(id);
     }
 
+    @PreAuthorize("hasRole('USER')")
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public void deleteBoard(@PathVariable Long id) {
         boardService.delete(id);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/{id}")
+    public void updateBoard(@LoginMember Member member, @RequestBody BoardDto boardDto, @PathVariable Long id) {
+        boardService.update(member, boardDto, id);
     }
 }
